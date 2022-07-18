1cfee4d1baf20cd0b4639ca4b0e03ae04811c003
Jeanne4L <100489924+Jeanne4L@users.noreply.github.com>
2022-07-18 17:08:07 +0200

    Correction du formulaire de contact

--- includes/form_1.php
+++ includes/form_1.php
@@ -1,23 +1,23 @@
 <?php	
-	if(empty($_POST['name']) && strlen($_POST['name']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['input_504']) && strlen($_POST['input_504']) == 0 || empty($_POST['message']) && strlen($_POST['message']) == 0)
+	if(empty($_POST['name']) && strlen($_POST['name']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['acquisition_channel']) && strlen($_POST['acquisition_channel']) == 0 || empty($_POST['project_description']) && strlen($_POST['project_description']) == 0)
 	{
 		return false;
 	}
 	
 	$name = $_POST['name'];
 	$email = $_POST['email'];
-	$input_504 = $_POST['input_504'];
-	$message = $_POST['message'];
+	$acquisitionChannel = $_POST['acquisition_channel'];
+	$projectDescription = $_POST['project_description'];
 	
 	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
 
 	// Create email	
 	$email_subject = "Message from your website";
 	$email_body = "You have received a new message. \n\n".
-				  "Name: $name \nEmail: $email \nInput_504: $input_504 \nMessage: $message \n";
+				  "Name: $name \nEmail: $email \nAcquisition_channel: $acquisitionChannel \nproject_description: $projectDescription \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
 	$headers .= "From: contact@yoursite.com\n";
-	$headers .= "Reply-To: $input_504";	
+	$headers .= "Reply-To: $acquisitionChannel";	
 	
 	mail($to,$email_subject,$email_body,$headers); // Post message
 	return true;			
